762cb6e823baee9fd5fc5e7a05e00e359e8eb032
Junwei Wu <1399952803@qq.com>
2015-07-08 23:23:15 +0800

    python study

---
+++ 201507/test01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# coding: utf-8
+
+print('中文')
+print('hello, %s') % 'world'
+print('Hi, %s, you have $%d.') %('wujunwei', 10000)
+print('%2d-%02d') %(3,1)
+print('%.2f') % 3.1415926
+print('Age: %s. Gender: %s') % (25, True)
+print('growth rate: %d %%') % 8
+
+classmates = ['michael', 'bob', 'tracy']
+print(classmates)
+print(len(classmates))
+print(classmates[0])
+# print(classmates[6])  #error 没有对应的数组元素
