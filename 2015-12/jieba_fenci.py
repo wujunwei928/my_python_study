21670fb2f08cf110aff4bd84f9c6ce03aac567f2
wujunwei <1399952803@qq.com>
2015-12-11 00:28:05 +0800

    jieba fenci test

--- 2015-12/jieba_fenci.py
+++ 2015-12/jieba_fenci.py
@@ -2,11 +2,19 @@
 # 如果上面不指定utf-8编码, 默认是ASCII编码, 下面分词会报错
 
 import jieba
+
+# 全模式
 seg_list = jieba.cut("我来到北京清华大学", cut_all=True)
-print "Full Mode:", "/ ".join(seg_list)  # 全模式
+print "Full Mode:", "/ ".join(seg_list)
+
+# 精确模式
 seg_list = jieba.cut("我来到北京清华大学", cut_all=False)
-print "Default Mode:", "/ ".join(seg_list)  # 精确模式
-seg_list = jieba.cut("他来到了网易杭研大厦")  # 默认是精确模式
+print "Default Mode:", "/ ".join(seg_list) 
+
+ # 默认是精确模式
+seg_list = jieba.cut("他来到了网易杭研大厦")
 print ", ".join(seg_list)
-seg_list = jieba.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")  # 搜索引擎模式
+
+# 搜索引擎模式
+seg_list = jieba.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")
 print ", ".join(seg_list)
