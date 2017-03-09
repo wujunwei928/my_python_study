cb5f8b49f17b585309ea4dde0294654765edd6ab
wujunwei <1399952803@qq.com>
2017-03-09 15:46:55 +0800

    插入排序原来修改表2中rank值的逻辑有问题, 修改之

--- insertSort/hive_transform/hive_transform.py
+++ insertSort/hive_transform/hive_transform.py
@@ -29,7 +29,15 @@ import sys
        
 if __name__=="__main__":
 
-    d3 = []
+    # 表1 中 part字段的 值
+    d1_part_value = 1
+
+    # record_list 用来记录表记录
+    record_list = []
+
+    # 记录每个topic_id对应的rank关系表, 用来推断表2中相应记录的rank变化
+    # 最初只记录 表1 中 相应topic_id对应的rank值列表, 后续会补充 表2 中 产生变化的rank值
+    rank_relationship = {}
     # 解析每一行数据
     for line in sys.stdin:
         # 略过空行
@@ -38,24 +46,47 @@ if __name__=="__main__":
 
         # 这里用try 避免特殊行解析错误导致全部出错
         try:
-            topic_id, sku_id, rank, part = line.strip().split("\t")
+            topic_id, sku_id, rank, part = [int(x) for x in line.strip().split("\t")]    # 保存的时候, 统一使用int值, 便于比较
         except:
             continue
+        # 保存相关记录
+        record_list.append({'topic_id':topic_id, 'sku_id':sku_id, 'rank':rank, 'part':part})
 
-        d3.append({'topic_id':topic_id, 'sku_id':sku_id, 'rank':rank, 'part':part})
+        # 保存表1 中 每个topic_id对应的rank值列表
+        if part == d1_part_value:
+            rank_relationship.setdefault(topic_id, [])
+            rank_relationship[topic_id].append( rank )
 
-    d1 = [x for x in d3 if int(x['part']) == 1]
+    # 将表1 和 表2 的数据分离出来
+    d1 = [x for x in record_list if int(x['part']) == d1_part_value]
+    d2 = [x for x in record_list if int(x['part']) != d1_part_value]
+    del(record_list)
 
-    for k1, v1 in enumerate(d1):
-        for k2, v2 in enumerate(d3):
-            v2['rank'] = int(v2['rank'])
-            if (v2['part'] != v1['part']) and (v2['topic_id'] == v1['topic_id']) and (v2['rank'] >= v1['rank']):
-                v2['rank'] += 1
-            d3[k2] = v2
+    # 针对表2对topic_id,rank值排序, 便于比较改变表2中相应的rank值
+    d2 = sorted(d2, key=lambda x:(x['topic_id'], x['rank']))    # python sorted 多个字段排序
 
-    # python sorted 多个字段排序
-    sorted_d3 = sorted(d3, key=lambda x:(x['topic_id'], x['rank']))
+    # # 这步比较建立在上面排序的基础上, 一定要先排序, 再进行下面改变rank值操作
+    for k2, v2 in enumerate(d2):
+        tmp_rank = v2['rank']
+        tmp_rank_list = rank_relationship.get(v2['topic_id'], [])
+        # 如果表2中记录的rank值在对应的rank列表中, 自增1, 直到不在对应的rank列表中
+        # 假设表1中topic_id=1 有对应的rank 1, 2, 3  表2中有1, 2, 3
+        # 1 在 [1, 2, 3] 中, 自增变为2, 2还在[1, 2, 3]中, 自增变为3, 3还在[1, 2, 3]中, 自增变为4, 4不在[1,2,3], 终止循环, 
+        # 同时将4补充到rank关系列表中, 变为[1, 2, 3, 4]
+        # 接下来比较2, 2在[1, 2, 3, 4]中, 自增为3, 3在[1, 2, 3, 4]中, 自增为4, 4在[1, 2, 3, 4]中, 自增为5
+        # 这里可以看到, 如果不将表2中对应变化的rank值补充到rank关系列表中, 后面判断可能会导致同一个topic_id下会出现相同的rank值
+        while tmp_rank in tmp_rank_list:
+            tmp_rank += 1
+        rank_relationship[v2['topic_id']].append(tmp_rank)    # 将变化的rank值保存起来, 便于后续rank值变化比较
+        d2[k2]['rank'] = tmp_rank
+    
+
+    # 对调整rank值之后, 两个表的数据再组合起来, 进行排序
+    d1.extend(d2)
+    del(d2)
+    sorted_record_list = sorted(d1, key=lambda x:(x['topic_id'], x['rank']))
 
     # 打印输出
-    for k, v in enumerate(sorted_d3):
-        print( str(v['topic_id']) + "\t" + str(v['sku_id']) + "\t" + str(v['rank']) )
+    for k, v in enumerate(sorted_record_list):
+        print('%d\t%d\t%d' %(v['topic_id'], v['sku_id'], v['rank']))
+
